b554063808865932faf4bf9389bbcf55a645b4d4
smartanil0000 <102318846+smartanil0000@users.noreply.github.com>
2022-06-20 23:33:30 +0530

    first commit

---
+++ client.js
@@ -0,0 +1,55 @@
+const socket = io()
+
+let nam;
+
+let textarea = document.querySelector('#textarea')
+
+let messageArea = document.querySelector('.message__area')
+do {
+  nam = prompt('please enter your name:')
+} while (!nam)
+
+textarea.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+    sendMessage(e.target.value)
+  }
+})
+
+function sendMessage(message) {
+  let msg = {
+    user: nam,
+    message: message.trim()
+  }
+
+  //append
+  appendMessage(msg, 'outgoing')
+  textarea.value = ''
+
+  scrollToBottom()
+
+  //send to server
+  socket.emit('message', msg)
+}
+
+function appendMessage(msg, type) {
+  let mainDiv = document.createElement('div')
+  let className = type
+  mainDiv.classList.add(className, 'message')
+
+  let markup = `
+    <h4>${msg.user}</h4>
+    <p>${msg.message}</p>
+    `
+  mainDiv.innerHTML = markup
+  messageArea.appendChild(mainDiv)
+}
+
+//received messages
+socket.on('message', (msg) => {
+  appendMessage(msg, 'incoming')
+  scrollToBottom()
+})
+
+function scrollToBottom(){
+    messageArea.scrollTop=messageArea.scrollHeight
+}
